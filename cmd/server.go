1da5fa7228069169f29dc7e6fe3f644c16361074
Tri Vuong <trivektor@gmail.com>
2022-06-07 16:56:28 +0700

    resume endpoints skeleton

---
+++ cmd/server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gorilla/mux"
+	"github.com/trivektor/gorm-resumes-api/pkg/database"
+	"github.com/trivektor/gorm-resumes-api/pkg/handlers"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "host=localhost user=root password=root dbname=postgres port=5432"
+	var err error
+
+	database.DB, err = gorm.Open(postgres.Open(dsn))
+
+	if (err != nil) {
+		log.Fatalln(err)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/resumes", handlers.GetResumes).Methods("GET")
+	r.HandleFunc("/resumes", handlers.CreateResume).Methods("POST")
+	r.HandleFunc("/resumes/{id}", handlers.GetResume).Methods("GET")
+	r.HandleFunc("/resumes/{id}", handlers.UpdateResume).Methods("PATCH")
+	r.HandleFunc("/resumes/{id}", handlers.DeleteResume).Methods("DELETE")
+}
---
+++ pkg/handlers/resumes.go
@@ -0,0 +1,23 @@
+package handlers
+
+import "net/http"
+
+func CreateResume(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetResumes(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetResume(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateResume(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteResume(w http.ResponseWriter, r *http.Request) {
+
+}
